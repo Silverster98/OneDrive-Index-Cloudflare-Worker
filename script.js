7f8f8a9279631f0f0a9c0c313033f29a62dc930c
Silvester98 <1539168414@qq.com>
2020-08-08 15:46:07 +0800

    fix: TypeError: {(intermediate value)} is not a function bug, and format doc

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 
- async function renderReadme() {
+async function renderReadme() {
     const resp = await fetch("readme.md")
-    if(!resp.ok) return
-    document.querySelector(".container").insertAdjacentHTML('beforeend','<div class="readme">'+window.marked(await resp.text()) + "</div>")
+    if (!resp.ok) return
+    document.querySelector(".container").insertAdjacentHTML('beforeend', '<div class="readme">' + window.marked(await resp.text()) + "</div>")
     Prism.highlightAll()
 
     // latex math render
@@ -11,43 +11,42 @@
             inlineMath: [['$', '$']]
         }
     }
-    
-    (function () {
-      var script = document.createElement('script')
-      script.src = "https://cdn.jsdelivr.net/npm/mathjax@3.0.1/es5/tex-mml-chtml.js"
-      script.async = true
-      document.head.appendChild(script)
-    })()
- }
+
+    let script = document.createElement('script')
+    script.src = "https://cdn.jsdelivr.net/npm/mathjax@3.0.1/es5/tex-mml-chtml.js"
+    script.async = true
+    document.head.appendChild(script)
+
+}
 
 
 function humanFileSize(bytes, si) {
-    bytes = parseInt(bytes,10)
+    bytes = parseInt(bytes, 10)
     var thresh = si ? 1000 : 1024;
-    if(Math.abs(bytes) < thresh) {
+    if (Math.abs(bytes) < thresh) {
         return bytes + ' B';
     }
     var units = si
-        ? ['kB','MB','GB','TB','PB','EB','ZB','YB']
-        : ['KiB','MiB','GiB','TiB','PiB','EiB','ZiB','YiB'];
+        ? ['kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
+        : ['KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB'];
     var u = -1;
     do {
         bytes /= thresh;
         ++u;
-    } while(Math.abs(bytes) >= thresh && u < units.length - 1);
-    return bytes.toFixed(1)+' '+units[u];
+    } while (Math.abs(bytes) >= thresh && u < units.length - 1);
+    return bytes.toFixed(1) + ' ' + units[u];
 }
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
 
-    document.querySelectorAll("a.item").forEach(function(i){
+    document.querySelectorAll("a.item").forEach(function (i) {
         i.insertAdjacentHTML('beforeend',
             '<div style="flex-grow:1"></div>'
-            + ( i.hasAttribute("size") ? '<span class="size">'+humanFileSize(i.getAttribute("size"),true)+'</span>' : '')
+            + (i.hasAttribute("size") ? '<span class="size">' + humanFileSize(i.getAttribute("size"), true) + '</span>' : '')
         )
     })
 
- });
+});
 
 import "https://cdn.jsdelivr.net/npm/marked/marked.min.js"
 
