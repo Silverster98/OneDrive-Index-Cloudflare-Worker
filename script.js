c18725192f16c8f1c5694e406b3ea5c3853789a2
Silvester98 <1539168414@qq.com>
2020-08-09 18:52:30 +0800

    feat: new version script.js

--- script.js
+++ script.js
@@ -1,25 +1,3 @@
-
-async function renderReadme() {
-    const resp = await fetch("readme.md")
-    if (!resp.ok) return
-    document.querySelector(".container").insertAdjacentHTML('beforeend', '<div class="readme">' + window.marked(await resp.text()) + "</div>")
-    Prism.highlightAll()
-
-    // latex math render
-    window.MathJax = {
-        tex: {
-            inlineMath: [['$', '$']]
-        }
-    }
-
-    let script = document.createElement('script')
-    script.src = "https://cdn.jsdelivr.net/npm/mathjax@3.0.1/es5/tex-mml-chtml.js"
-    script.async = true
-    document.head.appendChild(script)
-
-}
-
-
 function humanFileSize(bytes, si) {
     bytes = parseInt(bytes, 10)
     var thresh = si ? 1000 : 1024;
@@ -37,7 +15,21 @@ function humanFileSize(bytes, si) {
     return bytes.toFixed(1) + ' ' + units[u];
 }
 
+function addMathJaxLink() {
+    window.MathJax = {
+        tex: {
+            inlineMath: [['$', '$']]
+        }
+    }
+
+    let script = document.createElement('script')
+    script.src = "https://cdn.jsdelivr.net/npm/mathjax@3.0.1/es5/tex-mml-chtml.js"
+    script.async = true
+    document.head.appendChild(script)
+}
+
 document.addEventListener("DOMContentLoaded", function () {
+    if (window.location.pathname.endsWith('.md')) addMathJaxLink()
 
     document.querySelectorAll("a.item").forEach(function (i) {
         i.insertAdjacentHTML('beforeend',
@@ -47,31 +39,3 @@ document.addEventListener("DOMContentLoaded", function () {
     })
 
 });
-
-import "https://cdn.jsdelivr.net/npm/marked/marked.min.js"
-
-renderReadme().catch(console.error)
-
-function generatePath(path) {
-
-    const el = (tag, attrs, content) => `<${tag} ${attrs.join(" ")}>${content}</${tag}>`;
-    const div = (className, content) => el("div", [`class="${className}"`], content);
-
-    let items = path.split('/')
-    let link = ['']
-    let els = []
-
-    let i = 0
-    for (; i < items.length - 1; i++) {
-        link.push(link[i] + items[i] + '/')
-        els.push(el('a', [`href="${link[i+1]}"`], items[i] == '' ? '🚩 Home' : decodeURI(items[i])))
-    }
-    // if (items[i] !== '') link.push(link[i] + items[i])
-
-    if (items[i] !== '') els.push(decodeURI(items[i]))
-
-    // console.log(els)
-    return div('path', els.join(' / '))
-}
-
-document.getElementsByClassName('container')[0].insertAdjacentHTML('afterbegin', generatePath(window.location.pathname))
